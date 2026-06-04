b7dfc91a2896d12e468451fcd8e9d7f977800539
Deshawn <weathersdeshawn@gmail.com>
2026-06-03 22:13:27 -0700

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,9 @@ const ffmpeg = require("fluent-ffmpeg");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 
 // ── Directories ──────────────────────────────────────────────
